8dcc0ddb9fc4753ae6a92d74101f584769e70f2c
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-04-05 18:38:47 +0500

    Modernization Update

--- Automating Emails.py
+++ Automating Emails.py
@@ -1,14 +1,27 @@
-import smtplib
+# Python code to send an email using SMTP protocol and Gmail account
+# Importing necessary modules
+import smtplib as s
 from email.message import EmailMessage
-email = EmailMessage()  # Creating a object for EmailMessage
-email['from'] = 'xyz name'  # Person who is sending
-email['to'] = 'xyz id'  # Whom we are sending
-email['subject'] = 'xyz subject'  # Subject of email
-email.set_content("Xyz content of email")  # content of email
-with smtlib.SMTP(host='smtp.gmail.com', port=587)as smtp:
-    # sending request to server
-    smtp.ehlo()  # server object
-smtp.starttls()  # used to send data between server and client
-smtp.login("email_id", "Password")  # login id and password of gmail
-smtp.send_message(email)  # Sending email
-print("email send")  # Printing success message
+
+# Creating an EmailMessage object
+email = EmailMessage()
+
+# Setting up the email parameters
+email['from'] = 'xyz name' # Sender's name
+email['to'] = 'xyz id' # Recipient's email address
+email['subject'] = 'xyz subject' # Subject of email
+email.set_content("Xyz content of email") # Content of email
+
+# Creating an SMTP object and sending the email
+with s.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    # Greeting the server
+    smtp.ehlo()
+    # Establishing a secure connection with the server
+    smtp.starttls()
+    # Logging into the sender's Gmail account
+    smtp.login("email_id", "Password")
+    # Sending the email
+    smtp.send_message(email)
+
+# Printing success message
+print("Email sent successfully!")
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# PythonAutomatedEmails
+# Python Email Sending Script
+
+This is a simple Python script that sends an email using the smtplib library.
+
+## Prerequisites
+
+1. A Gmail account.
+2. Access to the internet.
+
+## Setup
+
+1. Clone the repository to your local machine.
+2. Open the file send_email.py in a text editor.
+3. Replace xyz name, xyz id, xyz subject, email_id, and Password with your own information.
+4. Save the file.
+
+## Usage
+
+1. Open a terminal or command prompt.
+2. Navigate to the directory where the send_email.py file is located.
+3. Run the command python send_email.py.
+4. Check the email address you specified to see if the email was sent successfully.
+
+## Notes
+
+* This script is designed to work with Gmail accounts, but can be modified to work with other email providers as well.
+* Be careful when entering your email login credentials. Keep your password private and do not share it with anyone.
